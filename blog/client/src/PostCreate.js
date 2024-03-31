faa4ee2b329313bb25faceb2738677289db6bc1e
luuthuan <lbthuan917@gmail.com>
2024-03-31 22:48:59 +0700

    refactor: Build React UI

---
+++ blog/client/src/PostCreate.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const PostCreate = () => {
+  const [title, setTitle] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post("http://localhost:4000/posts", {
+      title,
+    });
+
+    setTitle("");
+  };
+  return (
+    <div>
+      <form className="form-group mb-3" onSubmit={onSubmit}>
+        <div>
+          <label className="mb-2">Title</label>
+          <input
+            className="form-control"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default PostCreate;
