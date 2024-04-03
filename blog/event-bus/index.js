ea185cca46e6f8536af4629294a45908832c22db
luuthuan <lbthuan917@gmail.com>
2024-04-03 22:21:46 +0700

    refactor: Release code

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const axios = require("axios");
-const bodyParser = require("body-parser");
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.post("/event", (req, res) => {
   const event = req.body;
